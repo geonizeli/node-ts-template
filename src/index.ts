023d94bf63a09bbb1935194fe13310fc763a3816
João Geonizeli <joao.geonizeli@gmail.com>
2020-11-28 23:58:47 -0300

    config logger

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import 'dotenv/config'
+import logger from './logger'
 import server from './server'
 
 const httpPort = process.env.PORT
 
 server.listen(httpPort, () => {
-  console.error(`🚀 server up and running on port ${httpPort}`)
+  logger.info(`🚀 server up and running on port ${httpPort}`)
 })
